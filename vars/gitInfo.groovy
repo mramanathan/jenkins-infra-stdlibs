7d72ef0e6c0adc1673d2b4d1df1bb7f7052ee1f6
rmuthiah <ramanathan.muthiah@fireeye.com>
2017-03-21 19:22:22 +0530

    workflow lib : git

---
+++ vars/gitInfo.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+
+def call(body) {
+  // The call(body) method in any file in workflowLibs.git/vars is exposed as a
+  // method with the same name as the file.
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  sh "cd ${config.wsdir}"
+  sh "git config remote.origin.url"
+}
