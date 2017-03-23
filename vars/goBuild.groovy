a381370f8e6a58b3a0ad3fd7f79d770a957b9a0f
rmuthiah <ramanathan.muthiah@fireeye.com>
2017-03-24 02:30:36 +0530

    lib to build go code inside docker

---
+++ vars/goBuild.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/env groovy
+
+def call(body) {
+  // The call(body) method in any file in workflowLibs.git/vars is exposed as a
+  // method with the same name as the file.
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  node('docker') {
+    stage('Preparation...') {
+      deleteDir()
+    }
+    stage('SCM') {
+      checkout scm
+    }
+    stage('Go Build') {
+      docker.image(config.environment).inside {
+        sh "config.goBuild"
+      }
+    }
+  }
+}
