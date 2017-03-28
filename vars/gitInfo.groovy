3a2db11be4ba46338b8e62d68b9208c1f253cb6b
rmuthiah <ramanathan.muthiah@fireeye.com>
2017-03-28 22:04:54 +0530

    get remote name

--- vars/gitInfo.groovy
+++ vars/gitInfo.groovy
@@ -12,9 +12,10 @@ def call(body) {
   sh "cd ${config.wsdir}"
   // why isn't env.BRANCH_NAME working!!! ?
   def branch_name = sh(returnStdout: true, script: "git rev-parse --abbrev-ref HEAD").trim()
-  def origin_name = sh '''
-                       git rev-parse --abbrev-ref --symbolic-full-name ${branch_name}@{u} | awk -F'/' '{print $1}'
-                    '''
+  def origin_name = sh(returnStdout: true, script: "git remote").trim()
+  // def origin_name = sh '''
+  //                     git rev-parse --abbrev-ref --symbolic-full-name ${branch_name}@{u} | awk -F'/' '{print $1}'
+  //                  '''
   echo "${origin_name} is the remote for the branch, ${branch_name} and it's remote url is, "
   sh "git config remote.${origin_name}.url"
 }
