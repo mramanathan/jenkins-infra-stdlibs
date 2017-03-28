f3e54b4e1b6a3aa9b8e0bd088667f31ab3f3e1de
rmuthiah <ramanathan.muthiah@fireeye.com>
2017-03-29 02:22:08 +0530

    retrieve full git url of the remote

--- vars/gitInfo.groovy
+++ vars/gitInfo.groovy
@@ -16,22 +16,25 @@ def call(body) {
   // def origin_name = sh '''
   //                     git rev-parse --abbrev-ref --symbolic-full-name ${branch_name}@{u} | awk -F'/' '{print $1}'
   //                  '''
-  echo "${origin_name} is the remote for the branch, ${branch_name} and it's remote url is, "
-  sh "git config remote.${origin_name}.url"
+  echo "Gitlib :~> ${origin_name} is the remote for the branch, ${branch_name} and it's remote url is, "
+  // just 'remote' name is not sufficient to have git push the tags...this is the error in such scenario
+  // fatal: could not read Username for 'https://github.com': No such device or address
+  def remote_url = sh(script: "git config remote.${origin_name}.url", returnStdout: true).trim()
+  echo "Gitlib :~> ${remote_url}"
 
   // preparation for env for git operations...
   sh "git config user.email rus.cahimb@gmail.com"
   sh "git config user.name mramanathan"
 
   tagStatus = sh(script: "git tag -fa ${config.tagname} -m 'Git workflow library tag, ${config.tagname}'", returnStatus: true)
-  println "~> Status of git tagging: "
-  println "${tagStatus}"
+  println "Gitlib :~> Status of git tagging: "
+  println "Gitlib :~> ${tagStatus}"
 
   if ( tagStatus == 0 ) { 
-    echo "Pushing ${config.tagname} to ${origin_name}..."
-    sh "git push --tags --progress ${origin_name} ${config.tagname}"
+    echo "Gitlib :~> Pushing ${config.tagname} to ${origin_name}..."
+    sh "git push --tags --progress ${remote_url} ${config.tagname}"
   } else {
-    echo "Git tagging failed, nothing to push to the remote"
+    echo "Gitlib :~> Git tagging failed, nothing to push to the remote"
   }
 
 }
